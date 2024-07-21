858ea50dc90f1b2dcbc80c4c0c727b3f0e292d1d
Nhi Truong <nntruong2601@gmail.com>
2024-07-22 02:22:43 +0700

    Add files via upload

---
+++ Tornado-select-multiple-states.r
@@ -0,0 +1,59 @@
+library(maps)
+library(ggplot2)
+library(ggmap)
+library(plyr)
+library(animint2)
+load("UStornadoes.RData")
+ord <- order(unique(UStornadoes$TornadoesSqMile), decreasing=T)
+stateOrder <- data.frame(state = unique(UStornadoes$state)[ord], rank = 1:49)
+UStornadoes$state <-
+  factor(UStornadoes$state, levels=stateOrder$state, ordered=TRUE)
+UStornadoes$weight <- 1/UStornadoes$LandArea
+USpolygons <- map_data("state")
+USpolygons$state = state.abb[match(USpolygons$region, tolower(state.name))]
+UStornadoCounts <-
+  ddply(UStornadoes, .(state, year), summarize, count=length(state))
+seg.color <- "#55B1F7"
+years <- data.frame(year=unique(UStornadoes$year))
+states <- data.frame(state=unique(UStornadoes$state))
+
+tornado.viz <- list(
+  map=ggplot()+
+    theme_bw()+
+    theme_animint(width=750, height=500)+
+    geom_text(aes(
+      -100, 50, label=paste(
+        "Tornado paths and endpoints in ", year)),
+      showSelected="year",
+      data=years)+
+    geom_segment(aes(x=startLong, y=startLat, xend=endLong, yend=endLat),
+                     showSelected="year",
+                 colour=seg.color, data=UStornadoes)+
+    geom_point(aes(endLong, endLat), showSelected="year",
+               colour=seg.color,
+               fill=NA,
+               data=UStornadoes)+
+    geom_polygon(aes(x=long, y=lat, group=group), clickSelects="state",
+                 data=USpolygons, fill="grey", colour="black", alpha=3/4)+
+    theme(axis.line=element_blank(), axis.text=element_blank(),
+          panel.background=element_rect(color=NA),
+          panel.border=element_rect(color=NA),
+          axis.ticks=element_blank(), axis.title=element_blank()),
+  ts=ggplot()+
+    theme_bw()+
+    theme_animint(width=300, height=400)+
+    xlab("year")+
+    ylab("Number of tornadoes")+
+    geom_bar(aes(year, count), clickSelects="year", showSelected="state",
+             data=UStornadoCounts, stat="identity", position="identity")+
+    geom_text(aes(
+      1980, 200, label=paste("state =", state)), showSelected="state",
+      data=states)+
+    geom_text(aes(year, count + 5, label=count),
+                  showSelected2="year", showSelected="state",
+              data=UStornadoCounts, size=20))
+
+
+# Now create the animint visualization
+animint2dir(tornado.viz)
+
